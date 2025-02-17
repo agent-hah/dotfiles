a64c03ebde41e9c08b86562f98683b2148589707
Ashmit Handoo <ashmithandoo@outlook.com>
2025-02-17 02:23:44 +0000

    template make

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Add oh-my-zsh as a submodule if it's not there yet
+if [ ! -d "oh-my-zsh" ]; then
+  git submodule add https://github.com/ohmyzsh/ohmyzsh home/oh-my-zsh
+fi
+
+# Add devcontainer as submudle if it's not there yet
+if [ ! -d "devcontainer" ]; then
+  git submodule add https://github.com/dotfiles/.devcontainer home/.devcontainer
+# Get the latest oh-my-zsh
+git submodule init
+git submodule update --recursive --remote
+
+root=$(pwd)
+for file in $(ls home)
+do
+    source="$root/home/$file"
+    target="$HOME/.$file"
+
+    echo "Copying $source to $target"
+    cp -r $source $target
+done
